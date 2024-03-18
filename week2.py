24bcbe6741e49951d9d45b8f8b3cf38e30efe408
HaEinGodLs <godls031@naver.com>
2024-03-18 13:56:43 +0900

    add week2 homework

---
+++ week2.py
@@ -0,0 +1,59 @@
+
+## 1 ##
+
+print("100")
+print(100)
+print(50+50)
+print("50+50")
+
+## 2 ##
+print("\n")
+print("(2)")
+
+print('%d / %d = %d' % (5, 10, 5/10))
+
+## 3 ##
+print("\n")
+print("(3)")
+
+print("%04d" % 876)
+print("%8s" % "CookBook")
+print("%8s" % "Book")
+print("%1.1f" % 123.45)
+
+
+## 4 ##
+print("\n")
+print("(4)")
+
+print("{2:d} {0:d} {1:d}".format(111,222,333))
+
+
+## 13 ##
+print("\n")
+print("(13)")
+
+#print(int('1002', 2))
+#print(int('10', 2))
+
+#print(int('1008', 8))
+#print(int('AAFG', 16))
+
+
+## 15 ##
+print("\n")
+print("(15)")
+
+num = 12345678
+
+hex_num = hex(num)
+
+oct_num = oct(num)
+
+bin_num = bin(num)
+
+print("10진수 => ", num)
+print("16진수 => ", hex_num)
+print("8진수 => ", oct_num)
+print("2진수 => ", bin_num)
+
